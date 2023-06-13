09c421feecf99d96b96ca6fc874ad77663dbc1b4
Borhan Uddin Rabbani <130210717+borhanmukto@users.noreply.github.com>
2023-06-13 06:52:05 +0600

    Update Decision Tree Model.py

--- model creation/Decision Tree Model.py
+++ model creation/Decision Tree Model.py
@@ -10,7 +10,7 @@ y= df["Accident severity"]
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 encoder = OneHotEncoder(drop="first")
 
@@ -56,21 +56,3 @@ cr = classification_report(y_test_encoded, y_pred, zero_division=0)
 
 print(cr)
 
-# Seperate sensitivity and specificity for both classes
-
-sensitivity = cm.diagonal() / cm.sum(axis=1)
-
-
-specificity = []
-num_classes = len(le.classes_)
-for i in range(num_classes):
-    tn = np.delete(cm, i, axis=0).sum()  
-    fp = np.delete(cm[:, i], i, axis=0).sum()  
-    specificity.append(tn / (tn + fp))
-
-
-for i, class_name in enumerate(le.classes_):
-    print(f"Class: {class_name}")
-    print(f"Sensitivity: {sensitivity[i]}")
-    print(f"Specificity: {specificity[i]}")
-    print()
